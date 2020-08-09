c31daf35d3bc6084ee5006bb1663e35c36d2df24
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2020-08-09 20:40:03 -0300

    Analisando alguns filmes

---
+++ main.py
@@ -0,0 +1,125 @@
+import os
+import cv2
+import shutil
+import argparse
+
+import numpy as np
+import regex as re
+
+from sys import argv
+from functools import cmp_to_key
+from colorthief import ColorThief
+
+img_dump_path = './tmp'
+output_path   = './output'
+
+reg = r"(?<=frame)([0-9]+)(?=\.png)"
+
+def sorting_function ( a, b ):
+    an, bn = int(re.search(reg, a).group(0)), int(re.search(reg, b).group(0))
+    return an - bn
+
+def analyse_video ( video_path, frame_pace=1, num_dom_color=8, clean_tmp=True, skip_saving=False ):
+
+    if not skip_saving:
+
+        if not os.path.isfile(video_path):
+            raise Exception("Caminho não corresponde a um arquivo")
+        
+        cap = cv2.VideoCapture(video_path)
+
+        if not cap.isOpened(): 
+            print("Não foi possível abrir o vídeo.")
+        else:
+            length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            fps    = int(cap.get(cv2.CAP_PROP_FPS))
+
+            name   = os.path.basename(video_path).split('.')[0]
+            
+            print ( '> Total de frames.: %d' % length )
+            print ( '> Altura do frame.: %d' % height )
+            print ( '> Largura do frame: %d' % width )
+            print ( '> Framerate.......: %d' % fps )
+
+            current_frame = 0
+            success, frame = cap.read()
+            
+            if not os.path.isdir ( os.path.join ( img_dump_path, name )):
+                os.makedirs ( os.path.join ( img_dump_path, name ))
+
+            while current_frame < length and success :
+                cap.set ( 1, current_frame )
+                if not cv2.imwrite( os.path.join ( img_dump_path, name, 'frame%d.png' % current_frame ), frame ):
+                    raise Exception("Não foi possível escrever a imagem")
+                success, frame = cap.read()
+                current_frame += frame_pace
+
+            cap.release()
+            print('> Arquivos de frames salvos...')
+
+        
+    html  = "<html><body><title>Movie Palette</title>"
+    html += "<link rel='stylesheet' type='text/css' href='./style.css'><table><tr>"
+
+    for i in range(1, num_dom_color):
+        html += "<th>" + str(i) + "ª MCC</th>"
+
+    html += '</tr>'
+
+    for file in sorted(os.listdir(os.path.join ( img_dump_path, name )), key=cmp_to_key(sorting_function)):
+        colortf = ColorThief(os.path.join( img_dump_path, name, file))
+        dominant = colortf.get_palette(color_count=num_dom_color)
+        
+        title = re.search(reg, file)
+        
+        html += "<tr><td class='title'>" + title.group(0) + '</td>'
+        for color in dominant:
+            style = "'background-color:rgb(%d, %d, %d)'" % color
+            html += "<td style=" + style + "></td>"
+        html += '</tr>'
+
+    html += '</table></body></html>'
+
+    if not os.path.isdir ( os.path.join ( output_path, name )):
+        os.makedirs ( os.path.join ( output_path, name ))
+
+    file = open(os.path.join( output_path, name , 'index.html' ),'w')
+    file.write(html)
+    file.close()
+
+    if clean_tmp:
+        shutil.rmtree(os.path.join ( img_dump_path, name ))
+
+def main () :
+    parser = argparse.ArgumentParser(description='Video Color Analyser.', add_help=False)
+    parser.add_argument('-p', '--path', nargs=1, action='store', help='Caminho do arquivo do vídeo')
+    parser.add_argument('-f', '--frame-pace', nargs=1, action='store', help='Número de frames para pular')
+    parser.add_argument('-n', '--num-cor-dom', nargs=1, action='store', help='Número de cores dominantes')
+    parser.add_argument('-c', '--clean-files', action='store_true', help='Limpar os frames após processar')
+    parser.add_argument('-s', '--skip-saving', action='store_true', help='Skip saving images')
+    
+    args = parser.parse_args()
+
+    frame_pace = 1
+    num_dom_color = 8
+    clean_tmp = False
+
+    if not args.path:
+        print('Você deve fornecer um caminho para um vídeo')
+        return
+
+    if args.clean_files :
+        clean_tmp = True
+
+    if args.frame_pace:
+        frame_pace = args.frame_pace[0]
+    
+    if args.num_cor_dom:
+        num_dom_color = args.num_cor_dom[0]
+
+    analyse_video ( args.path[0], int(frame_pace), int(num_dom_color), clean_tmp )
+
+if __name__ == "__main__":
+    main()
